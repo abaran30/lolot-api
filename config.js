2a94f4c3b53d0e0cd4f11ffd89af7823eedb7ac5
Adrian Baran <adrian.baran.30@gmail.com>
2020-09-10 16:46:35 -0500

    fix: add new champion Yone

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   lolotOrigin: process.env.LOLOT_ORIGIN || 'undefined',
-  riotDDragonBaseUrl: process.env.RIOT_DDRAGON_BASE_URL || 'http://ddragon.leagueoflegends.com/cdn/10.1.1',
+  riotDDragonBaseUrl: process.env.RIOT_DDRAGON_BASE_URL || 'http://ddragon.leagueoflegends.com/cdn/10.18.1',
   riotGamesApiKey: process.env.RIOT_GAMES_API_KEY || 'RGAPI-fake-riot-games-api-key'
 };
